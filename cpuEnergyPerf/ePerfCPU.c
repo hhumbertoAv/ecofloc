0f96780835a0de587962816156dffdadc2ed8cb4
Hernan Humberto ALVAREZ VALERA <hhumberto.av@gmail.com>
2024-01-30 18:24:23 +0100

    Add files via upload

--- cpuEnergyPerf/ePerfCPU.c
+++ cpuEnergyPerf/ePerfCPU.c
@@ -1,5 +1,4 @@
 /*
-
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -16,13 +15,7 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
-
-
- */
-
-
-
-
+*/
 
 //TODO -> Calculate the energy of each process, considering the time of each step (is concerns) 
 
